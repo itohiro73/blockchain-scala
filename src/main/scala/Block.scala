ba279fc1ec3c3a0988ef868d37e71b530d41834c
Hiroshi Ito <itohiro73@gmail.com>
2018-11-08 21:01:25 +0900

    Implement Block.class.

--- src/main/scala/Block.scala
+++ src/main/scala/Block.scala
@@ -1,3 +1,18 @@
-class Block {
+import java.time.LocalDateTime
 
+class Block(val timestamp: LocalDateTime, val data: String, val previousHash: String = "") {
+  val hash = this.calculateHash()
+
+  def calculateHash(): String = {
+    val value = this.timestamp + this.data + this.previousHash
+    HashFunction.sha256(value)
+  }
+
+  override def toString: String = {
+    "{\n\tTimestamp: " + this.timestamp +
+    "\n\tData: " + this.data +
+    "\n\tPrevious Hash: " + this.previousHash +
+    "\n\tHash: " + this.hash +
+    "\n}"
+  }
 }
