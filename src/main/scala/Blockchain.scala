5805eb812728a10c0bbef39941b7c7ab03db7977
Hiroshi Ito <itohiro73@gmail.com>
2018-11-09 12:55:40 +0900

    Implement Blockchain.class.

--- src/main/scala/Blockchain.scala
+++ src/main/scala/Blockchain.scala
@@ -1,3 +1,29 @@
-class Blockchain {
+import java.time.LocalDateTime
 
+class Blockchain {
+  var chain = Seq[Block](createGenesisBlock())
+
+  def createGenesisBlock(): Block = {
+    new Block(LocalDateTime.now(), "A genesis block.")
+  }
+
+  def addBlock(block: Block): Unit = {
+    this.chain = this.chain :+ block
+  }
+
+  def getLastBlock(): Block = this.chain.last
+
+  def isChainValid(): Boolean = {
+    for(i <- 1 until this.chain.length) {
+      val currentBlock = this.chain(i)
+      val previousBlock = this.chain(i-1)
+      if(currentBlock.hash != currentBlock.calculateHash())
+        return false
+      if(currentBlock.previousHash != previousBlock.hash)
+        return false
+    }
+    return true
+  }
+
+  override def toString: String = this.chain.mkString("\n")
 }
